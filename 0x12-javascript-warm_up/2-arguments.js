64e938050775443839837b5c4f6878589b00d0da
Yassine <yassinesbi45@gmail.com>
2024-04-09 08:39:09 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
+
+// Get the number of arguments passed to the script
 const numArgs = process.argv.length - 2;
+
 if (numArgs === 0) {
-    console.log("No argument");
-  } else if (numArgs === 1) {
-    console.log("Argument found");
-  } else {
-    console.log("Arguments found");
-  }
+  console.log("No argument");
+} else if (numArgs === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
 
